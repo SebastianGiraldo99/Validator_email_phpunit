b24b5455a3c4e4b0911b4c1170e7046a103fba83
Sebastian Giraldo Guarin <sebastiangg990407@gmail.com>
2021-05-19 23:27:23 -0500

    First commit for the repositorie

---
+++ tests/ValidateTest.php
@@ -0,0 +1,17 @@
+<?php 
+
+use PHPUnit\Framework\TestCase;
+use App\Validate;
+
+
+class ValidateTest extends TestCase
+{
+    public function test_email()
+    {
+        $email = Validate::email("i@rimosoft.com");
+        $this->assertTrue($email);
+
+        $emailFalse = Validate::email("i@@rimosoft.com");
+        $this->assertFalse($emailFalse);
+    }
+}
